052c32fdcf31ea0aa5f0b00aef3d0081e2e4768c
rdemirian <rdemirian@scentregroup.com>
2023-11-25 04:27:35 +1100

    Create searchbutton component

---
+++ src/components/searchbutton/index.tsx
@@ -0,0 +1,17 @@
+import { Button } from '@chakra-ui/react';
+  
+  export const SearchButton: React.FC = () => (
+(
+    <Button
+    variant='solid'
+    size='lg'
+    colorScheme='linkedin'
+    sx={{ 
+      borderRadius: 10,
+      width: 40,
+    }}
+    >
+        Search
+    </Button>
+    )
+  )
